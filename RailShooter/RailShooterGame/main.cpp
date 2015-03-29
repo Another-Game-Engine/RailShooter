f90a8f0712619b2ca2c53554b48249cfd409d76e
Cesar Leblic <cesar.leblic@gmail.com>
2015-03-28 22:22:34 -0400

    Project configuration

--- RailShooter/RailShooterGame/main.cpp
+++ RailShooter/RailShooterGame/main.cpp
@@ -1,6 +1,47 @@
-#include <cstdlib>
+#include <stdlib.h>
+#include <Core/Engine.hh>
 
-int main(int ac, char **av)
+// DEPENDENCIES
+#include <Context/SDL/SdlContext.hh>
+#include <Core/ConfigurationManager.hpp>
+#include <Physic/BulletDynamicManager.hpp>
+#include <Core/Timer.hh>
+#include <AssetManagement/AssetManager.hh>
+#include <Core/ConfigurationManager.hpp>
+
+#ifdef USE_IMGUI
+#include <Utils/Age_Imgui.hpp>
+#endif
+
+
+#include <Threads/ThreadManager.hpp>
+#include <Threads/MainThread.hpp>
+#include <Core/DefaultConfiguration.hpp>
+#include <Threads/RenderThread.hpp>
+#include <Threads/Tasks/BasicTasks.hpp>
+
+using namespace AGE;
+
+int			main(int ac, char **av)
 {
-	return EXIT_SUCCESS;
+	InitAGE();
+	auto engine = CreateEngine();
+
+	engine->launch(std::function<bool()>([&]()
+	{
+		GetThreadManager()->setAsWorker(true, true, false);
+		engine->setInstance<Timer>();
+		engine->setInstance<AssetsManager>();
+
+		auto configurationManager = engine->getInstance<ConfigurationManager>();
+		configurationManager->setConfiguration<std::string>(std::string("ShadersPath"), std::string(engine->getApplicationPath() + "/../../../../GameEngine/Shaders/"));
+#ifdef USE_IMGUI
+		GetRenderThread()->getQueue()->emplaceFutureTask<Tasks::Basic::BoolFunction, bool>([=](){
+			Imgui::getInstance()->init(engine);
+			return true;
+		}).get();
+#endif
+		return true;
+	}));
+	return (EXIT_SUCCESS);
 }
